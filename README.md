81965323d69ee5a0e4df3489caad694f198bead2
Pedro Inurreta Palacio <pedroinupal@gmail.com>
2024-05-15 03:33:52 -0600

    Entrega Final de la materia Diseño Web

---
+++ README.md
@@ -0,0 +1,23 @@
+# DisenoWebProyecto
+Proyecto Final de la Materia Diseño Web
+
+# Cambios Evidencia 2:
+- Se agregarón los archivos de la aplicación. En ella están contenidas todas las migrations, factories, seeders, controllers y views que se crearon para esta entrega.
+  - Cabe recalcar que los botones y cualquier elemento interactivo aún no es funcional, pero las views de tablas si representan los datos creados por las factories en la base de datos.
+  - Las views creadas y sus rutas son las siguientes:
+    - **/login** (View para iniciar sesión)
+    - **/orders/new** (View para agregar una orden)
+    - **/employees/new** (View para agregar un empleado)
+    - **/customers/new** (View para agregar un cliente)
+    - **/suppliers/new** (View para agregar un proveedor)
+    - **/products/new** (View para agregar un producto al catálogo)
+    - **/products/buy** (View para comprar producto del catálogo)
+    - **/orders_all** (View para ver las ordenes, diseñado para empleados con rol de sales)
+    - **/orders_warehouse** (View para ver las ordenes, diseñado para empleados con rol de Purchasing o Warehouse)
+    - **/orders_route** (View para ver las ordenes, diseñado para empleados con rol de Route)
+    - **/employees** (View para ver los empleados registrados) 
+    - **/customers** (View para ver los clientes registrados)
+    - **/suppliers** (View para ver los proveedores)
+    - **/products** (View para ver los productos)
+      
+- Al desarrollar la aplicación vimos que faltaban algunas tablas/atributos para que la aplicación tuviera sentido. El diagrama de E-R fue actualizado para reflejar estos cambios y puede ser encontrado en la carpeta de diagrams.
---
+++ halcon-app/resources/views/auth/login.blade.php
@@ -0,0 +1,57 @@
+<head>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+</head>
+<body>
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header">{{ __('Login') }}</div>
+
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('login') }}">
+                            @csrf
+
+                            <!-- Email Address -->
+                            <div class="mb-3">
+                                <label for="email" class="form-label">{{ __('Email') }}</label>
+                                <input id="email" class="form-control" type="email" name="email" :value="old('email')" required autofocus autocomplete="username">
+                                @error('email')
+                                    <div class="text-danger">{{ $message }}</div>
+                                @enderror
+                            </div>
+
+                            <!-- Password -->
+                            <div class="mb-3">
+                                <label for="password" class="form-label">{{ __('Password') }}</label>
+                                <input id="password" class="form-control" type="password" name="password" required autocomplete="current-password">
+                                @error('password')
+                                    <div class="text-danger">{{ $message }}</div>
+                                @enderror
+                            </div>
+
+                            <!-- Remember Me -->
+                            <div class="mb-3 form-check">
+                                <input id="remember_me" type="checkbox" class="form-check-input" name="remember">
+                                <label for="remember_me" class="form-check-label">{{ __('Remember me') }}</label>
+                            </div>
+
+                            <div class="d-grid">
+                                <button type="submit" class="btn btn-primary">{{ __('Log in') }}</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                @if (Route::has('password.request'))
+                    <div class="mt-3 text-center">
+                        <a href="{{ route('password.request') }}" class="text-sm text-gray-600 hover:text-gray-900">{{ __('Forgot your password?') }}</a>
+                    </div>
+                @endif
+            </div>
+        </div>
+    </div>
+</body>
+</html>
